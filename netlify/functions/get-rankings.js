65014ed5cc9cc2bbfbd9dd7002c8fa83f634e093
jhogya <joseph@apcera.com>
2026-09-09 22:57:41 -0700

    Update get-rankings.js

--- netlify/functions/get-rankings.js
+++ netlify/functions/get-rankings.js
@@ -2,7 +2,11 @@ const { getStore } = require('@netlify/blobs');
 const { STORE_NAME, HISTORY_KEY, LATEST_KEY } = require('./lib/refresh');
 
 exports.handler = async () => {
-  const store = getStore(STORE_NAME);
+  const store = getStore({
+    name: STORE_NAME,
+    siteID: process.env.NETLIFY_SITE_ID,
+    token: process.env.NETLIFY_API_TOKEN,
+  });
   const latest = await store.get(LATEST_KEY, { type: 'json' });
   const history = await store.get(HISTORY_KEY, { type: 'json' });
 
